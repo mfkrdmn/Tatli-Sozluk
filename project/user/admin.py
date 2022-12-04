1432f25ed0ab750aa0a266d8ceb9068e439bd9e1
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-12-04 12:51:44 +0300

    user model done

--- project/user/admin.py
+++ project/user/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'nick', 'is_active')
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(User,  CustomUserAdmin)
+admin.site.register(UserProfile)
