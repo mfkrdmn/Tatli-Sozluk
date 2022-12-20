08e5ad6bb905c158201e217174ab30c4b3db6bfe
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-12-20 17:43:03 +0300

    pagination done for home page

--- project/homepage/views.py
+++ project/homepage/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from entry.models import *
+from django.core.paginator import Paginator
 # Create your views here.
 
 def home(request):
     all_entries = Entry.objects.all().order_by('-created_at')
     first_comment = EntryComments.objects.all().order_by('-created_at')
 
+    #Paginator
+
+
+    paginator = Paginator(first_comment, 10) # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    #
+
     context = {
         'all_entries' : all_entries,
-        'first_comment' :first_comment,
+        'page_obj': page_obj
     }
     return render(request, 'home.html', context)
 
